ef748cbdb0512fec45ad0174730d0b7e66ce9b34
monem2077 <mohamed.abdelmonem@espace.com.eg>
2021-10-29 11:52:58 +0200

    Binary Search

---
+++ binary_search.js
@@ -0,0 +1,11 @@
+let binary_search = function (nums, target, low = 0, high = nums.length - 1) {
+    if (low > high) return -1;
+
+    let pivot = Math.round((low + high) / 2);
+
+    if (nums[pivot] == target) return pivot;
+    else if (nums[pivot] < target) return binary_search(nums, target, pivot + 1, high);
+    else return binary_search(nums, target, low, pivot - 1);
+}
+
+console.log(binary_search([-1,0,3,5,9,12], 2))
