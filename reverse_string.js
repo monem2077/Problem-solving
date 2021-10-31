739769e22b337a8a3e82798aac5fbbedb60a705e
monem2077 <mohamed.abdelmonem@espace.com.eg>
2021-10-31 11:13:49 +0200

    Reverse String recurssion

--- reverse_string.js
+++ reverse_string.js
@@ -1,16 +1,18 @@
 var reverseString = function(s) {
-    let  i = 0;
-    let j = s.length - 1;
-
-    while (i < j) {
-        let tmp = s[i];
-        s[i] = s[j];
-        s[j] = tmp;
-        i++;
-        j--;
-    }
-
-    return s;
+    return reverse(s)
 };
+
+const reverse = function(s, low = 0, high = s.length - 1) {
+
+    if (low >= high) return s;
+    let tmp = s[low];
+    s[low] = s[high];
+    s[high] = tmp;
+    low++;
+    high--;
+    return reverse(s, low ,high);
+}
+
+reverseString(["h","e","l","l","o"]);
 
 // https://leetcode.com/problems/reverse-string/.
